627dfa679b0090dda0d8d9b0de5f148d9e733537
NandeMbixo <mbxnan001@myuct.ac.za>
2024-06-18 12:50:22 +0200

    Initialise Nande's New Version

---
+++ react-admin-dashboard-master/src/scenes/faq/survey_json.js
@@ -0,0 +1,335 @@
+export const json = {
+  
+    "pages": [
+     {
+      "name": "page1",
+      
+
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question1",
+        "category": "forming",
+        "title": "We try to have set procedures or protocols to ensure that things are orderly and run smoothly (e.g. minimize interruptions; everyone gets the opportunity to have their say). \n",
+        
+        "isRequired": true
+
+       },
+       {
+         "type": "rating",
+         "name": "question2",
+         "category": "storming",
+         "title": "We are quick to get on with the task on hand and do not spend too much time in the planning stage.",
+         "isRequired": true,
+        
+        },
+       {
+        "type": "rating",
+        "name": "question3",
+        "category": "performing",
+        "title": "Our team feels that we are all in it together and shares responsibilities for the team's success or failure.",
+      
+        "isRequired": true
+       },
+       {
+         "type": "rating",
+         "name": "question4",
+         "category": "norming",
+         "title": "We have thorough procedures for agreeing on our objectives and planning the way we will perform our tasks.",
+        
+         "autoGenerate": true,
+         "isRequired": true
+        },
+        {
+         "type": "rating",
+         "name": "question5",
+         "category": "forming",
+         "title": "Our team feels that we are all in it together and shares responsibilities for the team's success or failure.",
+         
+         "isRequired": true
+        },
+
+
+
+
+      ],
+
+      "title": "Did You Have Fun In The Last Sprint?",
+      "description": "Response Scale\r\n1 - Almost never\r\n2 - Seldom\r\n3 -  Occasionally\r\n4 - Frequently\r\n5 - Almost always"
+     
+     },
+
+     
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question6",
+        "category": "norming",
+        "title": "We take our team's goals and objectives literally, and assume a shared understanding",
+      
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question7",
+        "category": "storming",
+        "title": "The team leader tries to keep order and contributes to the task at hand.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question8",
+        "category": "performing",
+        "title": "We do not have fixed procedures, we make them up as the task or project progresses.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question9",
+        "category": "storming",
+        "title": "We generate lots of ideas, but we do not use many because we fail to listen to them and reject them without fully understanding them.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question10",
+        "category": "forming",
+        "title": "Team members do not fully trust the other team members and closely monitor others who are working on a specific task.",
+       
+        "isRequired": true
+       }
+      ],
+
+      "title": "How Would You Rate the Team's Productivity in the Last Sprint?",
+      "description": "Response Scale\r\n1 - Almost never\r\n2 - Seldom\r\n3 -  Occasionally\r\n4 - Frequently\r\n5 - Almost always"
+     
+     },
+     {
+      "name": "page3",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question11",
+        "category": "norming",
+        "title": "The team leader ensures that we follow the procedures, do not argue, do not interrupt, and keep to the point.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question12",
+        "category": "performing",
+        "title": "We enjoy working together; we have a fun and productive time.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question13",
+        "category": "norming",
+        "title": "We have accepted each other as members of the team.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question14",
+        "category": "performing",
+        "title": "The team leader is democratic and collaborative.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question15",
+        "category": "forming",
+        "title": "We are trying to define the goal and what tasks need to be accomplished.\n",
+       
+        "isRequired": true
+       }
+      ],
+      "title": "How Collaborative is the Team?",
+      "description": "Response Scale\r\n1 - Almost never\r\n2 - Seldom\r\n3 -  Occasionally\r\n4 - Frequently\r\n5 - Almost always"
+     
+     },
+     {
+      "name": "page4",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question16",
+        "category": "storming",
+        "title": "Many of the team members have their own ideas about the process and personal agendas are rampant.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question17",
+        "category": "performing",
+        "title": "We fully accept each other's strengths and weaknesses.\n",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question18",
+        "category": "forming",
+        "title": "We assign specific roles to team members (team leader, facilitator, time keeper, note taker, etc.)",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question19",
+        "category": "norming",
+        "title": "We try to achieve harmony by avoiding conflict.",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question20",
+        "category": "storming",
+        "title": "The tasks are very different from what we imagined and seem very difficult to accomplish",
+       
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question21",
+        "category": "forming",
+        "title": " There are many abstract discussions of the concepts and issues, which make some members impatient with these discussions",
+       
+        "isRequired": true
+       }
+      ],
+      "title": "Did You Have Fun In The Last Sprint?",
+      "description": "Response Scale\r\n1 - Almost never\r\n2 - Seldom\r\n3 -  Occasionally\r\n4 - Frequently\r\n5 - Almost always"
+     
+     },
+     {
+        "name": "page5",
+        "elements": [
+         {
+          "type": "rating",
+          "name": "question22",
+          "category": "performing",
+          "title": "We are able to work through group problems.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question23",
+          "category": "storming",
+          "title": "We argue a lot even though we agree on the real issues.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question24",
+          "category": "norming",
+          "title": "The team is often tempted to go above the original scope of the project.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question25",
+          "category": "norming",
+          "title": "We express criticism of others constructively",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question26",
+          "category": "performing",
+          "title": "There is a close attachment to the team.",
+         
+          "isRequired": true
+         },
+         {
+            "type": "rating",
+            "name": "question27",
+            "category": "forming",
+            "title": "It seems as if little is being accomplished with the project's goals.",
+           
+            "isRequired": true
+           },
+        
+        ],
+        "title": "Do You Feel That We are Delivering Value to our Customer?",
+      "description": "Response Scale\r\n1 - Almost never\r\n2 - Seldom\r\n3 -  Occasionally\r\n4 - Frequently\r\n5 - Almost always"
+     
+       },
+       {
+        "name": "page6",
+        "elements": [
+         {
+          "type": "rating",
+          "name": "question28",
+          "category": "storming",
+          "title": "The goals we have established seem unrealistic.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question29",
+          "category": "forming",
+          "title": "Although we are not fully sure of the project's goals and issues, we are excited and proud to be on the team.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question30",
+          "category": "norming",
+          "title": "We often share personal problems with each other.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question31",
+          "category": "storming",
+          "title": "There is a lot of resisting of the tasks on hand and quality improvement approaches.",
+         
+          "isRequired": true
+         },
+         {
+          "type": "rating",
+          "name": "question32",
+          "category": "performing",
+          "title": "We get a lot of work done.",
+         
+          "isRequired": true
+         },
+         
+      ],
+      "title": "Are You Comfortable with Your Current Workload, or Do You Feel Overwhelmed?",
+      "description": "Response Scale\r\n1 - Almost never\r\n2 - Seldom\r\n3 -  Occasionally\r\n4 - Frequently\r\n5 - Almost always"
+     
+      
+     }
+    ],
+    "sendResultOnPageNext": true,
+    "questionsOrder": "random",
+    "showProgressBar": "aboveheader",
+    "questionErrorLocation": "bottom",
+    "progressBarShowPageNumbers": true,
+    "progressBarInheritWidthFrom": "survey",
+    "maxTimeToFinish": 300,
+    "showTimerPanel": "top",
+    "showTimerPanelMode": "survey",
+    "widthMode": "responsive",
+    "headerView": "basic"
+   }
